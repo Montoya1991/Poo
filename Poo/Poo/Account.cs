9e812931ab86e6b3a5e2311cbfa4aa1b2e487361
Toya <jdmontoya0491@hotmail.com>
2023-06-07 14:41:21 -0500

    Creacion de clases

---
+++ Poo/Poo/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    internal class Account
+    {
+        int id;
+        string name;
+        string document;
+        string email;
+        string password;
+            }
+}
